92e0e07707d4145a8987a693c550c9f080ab4406
Artit Attasiri <atit.att@ascendcorp.com>
2018-09-20 14:40:33 +0700

    add repository and set time to live

---
+++ src/main/java/com/example/springredis/configuration/redis/RedisProperties.java
@@ -0,0 +1,4 @@
+package com.example.springredis.configuration.redis;
+
+public class RedisProperties {
+}
---
+++ src/main/java/com/example/springredis/repository/UserRepositoryImpl.java
@@ -0,0 +1,4 @@
+package com.example.springredis.repository;
+
+public class UserRepositoryImpl {
+}
